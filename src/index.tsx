df660174c80e4225b69695c7cd488371a6a35e44
renemonroy <rene.monroy@gmail.com>
2021-04-06 00:15:26 -0500

    Export components created and their respective types

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Dot from "./components/Dot/Dot";
+import * as DotTypes from "./components/Dot/Dot.types";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import DotsList from "./components/DotsList/DotsList";
+import * as DotsListTypes from "./components/DotsList/DotsList.types";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { Dot, DotTypes, DotsList, DotsListTypes };
