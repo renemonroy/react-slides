1bfe79bce1f9b9d6a37f77f656c78ef8b8bdc2e8
renemonroy <rene.monroy@gmail.com>
2021-04-12 14:36:51 -0500

    Use black dots

--- src/components/Slides/Slides.stories.tsx
+++ src/components/Slides/Slides.stories.tsx
@@ -77,7 +77,7 @@ const Template: Story<ISlides> = (args) => (
 export const WithGap = Template.bind({});
 WithGap.args = {
   disableDots: true,
-  dotsColor: "#010101",
+  dotsColor: "#000000",
   items: [
     { text: "First" },
     { text: "Second" },
