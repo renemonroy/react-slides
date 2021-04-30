a25bfce481252d7e6c4a0fa17b9845c18279241b
René Monroy <rene.monroy@gmail.com>
2021-04-30 13:18:28 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import Dot from "./components/Dot/Dot";
 import * as DotTypes from "./components/Dot/Dot.types";
-
 import DotsList from "./components/DotsList/DotsList";
 import * as DotsListTypes from "./components/DotsList/DotsList.types";
 
